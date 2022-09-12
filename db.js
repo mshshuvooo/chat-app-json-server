a74181ba562021ccd0841f87e32747b63416d5b8
Shuvo <shuvombm02@gmail.com>
2022-09-12 21:59:09 +0600

    change server.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const users = require("./data/users");
 const conversations = require("./data/conversations");
-const messages = require("./data/messages");;
+const messages = require("./data/messages");
 
 module.exports = {
     users,
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
+const data = require("./db.js");
 const auth = require("json-server-auth");
 const jsonServer = require("json-server");
-
 const server = jsonServer.create();
-const router = jsonServer.router("db.js");
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
-// Bind the router db to the app
 server.db = router.db;
 
 server.use(middlewares);
